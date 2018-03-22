6d24fdcdd1913c14ceceb814b1fb7dcfcd7505c9
FuckingHacking <navchalkin@ex.ua>
2018-03-22 09:25:00 +0200

    add countdown

--- js/clock.js
+++ js/clock.js
@@ -16,11 +16,14 @@ var duration = moment.duration(diffTime, 'milliseconds', true);
 // set interval to milliseconds
 var interval = 1000;
 
+
 setInterval(function(){
   duration = moment.duration(duration - interval, 'milliseconds');
   $('#clock').html(
-      "<div class=\'days cell\' style=\'color: #3b3f40;\'>"+duration.days()+"<span style=\'color: #babcbe;\'>days</span></div>" +
-      "<div class=\'hours cell\' style=\'color: #3b3f40;\'>"+duration.hours()+"<span style=\'color: #babcbe;\'>hours</span></div>" +
-      "<div class=\'mins cell\' style=\'color: #3b3f40;\'>"+duration.minutes()+"<span style=\'color: #babcbe;\'>mins</span></div>" +
-      "<div class=\'secs cell\' style=\'color: #3b3f40;\'>"+duration.seconds()+"<span style=\'color: #babcbe;\'>secs</span></div>")
+      "<div class=\'days cell\' style=\'color: #3b3f40;\'>"+duration.days()+"<span style=\'color: #babcbe;\'>дні</span></div>" +
+      "<div class=\'hours cell\' style=\'color: #3b3f40;\'>"+duration.hours()+"<span style=\'color: #babcbe;\'>год</span></div>" +
+      "<div class=\'mins cell\' style=\'color: #3b3f40;\'>"+duration.minutes()+"<span style=\'color: #babcbe;\'>хв</span></div>" +
+      "<div class=\'secs cell\' style=\'color: #3b3f40;\'>"+duration.seconds()+"<span style=\'color: #babcbe;\'>сек</span></div>")
 }, interval);
+
+
